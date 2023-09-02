04a6f69b6d92f861d172e12f828d29b8fabb14b1
Gaurav Arya <gauravarya272@gmail.com>
2023-09-02 14:33:07 -0400

    could not deduce type of integer

--- examples/Robot1/main_error1.cpp
+++ examples/Robot1/main_error1.cpp
@@ -1,26 +1,10 @@
-
-// this MUST be first, otherwise there might be problems on windows
-// see: https://stackoverflow.com/questions/6563810/m-pi-works-with-math-h-but-not-with-cmath-in-visual-studio/6563891#6563891
 #define _USE_MATH_DEFINES
 #include <cmath>
-
-#define EIGEN_USE_BLAS
-
-#include "SystemModel.hpp"
-
-#include <kalman/ExtendedKalmanFilter.hpp>
-
 #include <iostream>
-#include <random>
-#include <chrono>
-
 #include <Eigen/Core>
+
 extern double __enzyme_autodiff(void *, double);
-
-using namespace KalmanExamples;
-
 typedef float T;
-
 typedef Eigen::Matrix<T, 3, 1> State;
 
 class LinearizedSystemModel
@@ -47,14 +31,7 @@ double simulate(double input) {
 }
 
 int main(int argc, char **argv) {
-    double x1 = simulate(1.0);
-    double x2 = simulate(1.1);
-    std::cout << "x1: " << x1 << ", x2: " << x2 << std::endl;
-
-    double df_dx1 = __enzyme_autodiff((void *)simulate, 1.0);
-    double df_dx2 = __enzyme_autodiff((void *)simulate, 1.1);
-    printf("x = %f, f(x) = %f, f'(x) = %f", 1.0, x1, df_dx1);
-    printf("x = %f, f(x) = %f, f'(x) = %f", 1.1, x2, df_dx2);
+    double df_dx = __enzyme_autodiff((void *)simulate, 1.0);
 
     return 0;
 }
