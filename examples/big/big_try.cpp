f3d145de4983e3a3608418752b6011135fd49df5
William S. Moses <gh@wsmoses.com>
2023-10-22 00:47:03 -0400

    Minified to success

--- examples/big/big_try.cpp
+++ examples/big/big_try.cpp
@@ -1,134 +1,22 @@
-// this MUST be first, otherwise there might be problems on windows
-// see: https://stackoverflow.com/questions/6563810/m-pi-works-with-math-h-but-not-with-cmath-in-visual-studio/6563891#6563891
 #define _USE_MATH_DEFINES
 #include <cmath>
 
 #define EIGEN_USE_BLAS
 
-#include "big.hpp"
-
-#include <kalman/ExtendedKalmanFilter.hpp>
-
-#include <iostream>
-#include <random>
-#include <chrono>
-
-#include <Eigen/Core>
-extern int enzyme_dup;
-extern int enzyme_dupnoneed;
-extern int enzyme_out;
-extern int enzyme_const;
-
-void __enzyme_autodiff(...);
+#include <Eigen/Dense>
 
 template<typename RT, typename... Args>
 RT __enzyme_autodiff(void*, Args...);
 
-using namespace KalmanExamples;
-
-typedef float T;
-
-typedef Big::State<T> State;
-typedef Big::Control<T> Control;
-typedef Big::SystemModel<T> SystemModel;
-
-const size_t n = Big::n;
-
-// typedef Big::PositionMeasurement<T> PositionMeasurement;
-// typedef Big::OrientationMeasurement<T> OrientationMeasurement;
-// typedef Big::PositionMeasurementModel<T> PositionModel;
-
-typedef Big::MeasurementModel<T> MeasurementModel;
-typedef Big::Measurement<T> Measurement;
-
-double simulate(double* A) {//Kalman::Jacobian<State, State> A) {
-  // init state
-  State x;
-  for (int i = 0; i < n; i++) {
-    x[i] = 1;
-  }
-  
-  // init control
-  Control u;
-  u[0] = 0.0;
-
-  // init measurement
-  MeasurementModel mm;
-
-  // init system
-  SystemModel sys;
-
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      sys.F(i, j) = A[n * i + j];
-    }
-  }
-
-  // init ekf
-  Kalman::ExtendedKalmanFilter<State> ekf;
-  ekf.init(x);
-  ekf.P.setIdentity(); // explicitly set initial covariance, although identity is secretly already the default
-
-  double error_sum = 0.0;
-  const size_t N = 5;
-
-  for (size_t i = 1; i <= N; i++) {
-
-    // propagate hidden state
-    x = sys.f(x, u);
-
-    // propagate state estimate, read out mean
-    State x_ekf = ekf.predict(sys, u);
-
-    // measurement
-    Measurement m = mm.h(x);
-    ekf.update(mm, m);
-
-    error_sum += std::pow(x[0], 2); 
-    // error_sum += std::pow(x_ekf[0] - x[0], 2); 
-    // add a funky P-dependent term to test differentiation
-    // error_sum += std::pow(ekf.P(0,0), 2); 
-
-    // std::cout << x[0] << "," << x[1] << "," << x_ekf[0]
-    //           << "," << x_ekf[1] << std::endl;
-  }
-  return error_sum / (double)N;
+double simulate() {
+  Eigen::Matrix<float, 5, 5> H;
+  H.setIdentity();
+  auto K = H.inverse();
+  return K(0,0);
 }
 
 int main(int argc, char **argv) {
-
-    // Kalman::Jacobian<State, State> A;
-    // Kalman::Jacobian<State, State> Adup;
-
-    double A[n * n];
-    double Adup[n * n];
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            A[n*i + j] = 1.0;
-            Adup[n*i + j] = 0.0;
-        }
-    }
-   
-
-    double delta = 0.001;
-    // double fx1 = simulate(1.0, A);
-    // double fx2 = simulate(1.0 + delta, A);
-    // std::cout << "fx1: " << fx1 << ", fx2: " << fx2 << std::endl;
-
-    // double df_dx1 = __enzyme_autodiff<double>((void *)simulate, enzyme_out, 1.0, enzyme_const, A);
-    // double df_dx2 = __enzyme_autodiff<double>((void *)simulate, enzyme_out, 1.0 + delta, enzyme_const, A);
-    // printf("x = %f, f(x) = %f, f'(x) = %f, f'(x) fd = %f\n", 1.0, fx1, df_dx1, (fx2 - fx1) / delta);
-    // printf("x = %f, f(x) = %f, f'(x) = %f", 1.0 + delta, fx2, df_dx2);
-
-    double fx1 = simulate(A);
-    A[0] += delta;
-    double fx2 = simulate(A);
-    A[0] -= delta;
-    printf("f(A) = %f, f(A + delta) = %f, f'(A)[0, 0] fd = %f\n", fx1,  fx2,(fx2 - fx1) / delta);
-
-    __enzyme_autodiff<double>((void *)simulate, enzyme_dup, A, Adup);
-    printf("Adup[0, 0] = %f, Adup[0, 1] = %f", Adup[0], Adup[1]);
+    __enzyme_autodiff<double>((void *)simulate);
 
     return 0;
 }
