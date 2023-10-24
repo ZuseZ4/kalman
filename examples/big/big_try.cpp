0dc936d36d0c795dccc0bff5994c70b7083debe3
Gaurav Arya <gauravarya272@gmail.com>
2023-10-24 16:48:24 -0400

    Correctness issue 1

--- examples/big/big_try.cpp
+++ examples/big/big_try.cpp
@@ -126,16 +126,34 @@ int main(int argc, char **argv) {
     A[0] += delta;
     double fx2 = simulate(A);
     A[0] -= delta;
-    printf("f(A) = %f, f(A + delta) = %f, f'(A)[0, 0] fd = %f\n", fx1,  fx2,(fx2 - fx1) / delta);
+    printf("f(A) = %f, f(A + delta) = %f, f'(A)[0] fd = %f\n", fx1,  fx2,(fx2 - fx1) / delta);
 
     fx1 = simulate(A);
     A[0] += delta2;
     fx2 = simulate(A);
     A[0] -= delta2;
-    printf("f(A) = %f, f(A + delta2) = %f, f'(A)[0, 0] fd = %f\n", fx1,  fx2,(fx2 - fx1) / delta2);
+    printf("f(A) = %f, f(A + delta2) = %f, f'(A)[0] fd = %f\n", fx1,  fx2,(fx2 - fx1) / delta2);
+
+    fx1 = simulate(A);
+    A[1] += delta;
+    fx2 = simulate(A);
+    A[1] -= delta;
+    printf("f(A) = %f, f(A + delta) = %f, f'(A)[1] fd = %f\n", fx1,  fx2,(fx2 - fx1) / delta);
+
+    fx1 = simulate(A);
+    A[2] += delta;
+    fx2 = simulate(A);
+    A[2] -= delta;
+    printf("f(A) = %f, f(A + delta) = %f, f'(A)[2] fd = %f\n", fx1,  fx2,(fx2 - fx1) / delta);
+
+    fx1 = simulate(A);
+    A[10] += delta;
+    fx2 = simulate(A);
+    A[10] -= delta;
+    printf("f(A) = %f, f(A + delta) = %f, f'(A)[10] fd = %f\n", fx1,  fx2,(fx2 - fx1) / delta);
 
     __enzyme_autodiff<double>((void *)simulate, enzyme_dup, A, Adup);
-    printf("Adup[0, 0] = %f, Adup[0, 1] = %f", Adup[0], Adup[1]);
+    printf("Adup[0] = %f, Adup[1] = %f, Adup[2] = %f, Adup[10] = %f", Adup[0], Adup[1], Adup[2], Adup[10]);
 
     return 0;
 }
